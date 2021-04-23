cb46ea7db0a6f924f271e5416c8db0c5c5844194
Fabien Rondeau <fabien.rondeau1@leroymerlin.fr>
2021-04-23 15:53:58 +0200

    push final

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,7 @@ console.log('Script started successfully');
 /* 
  * Handle the chat
  */
-WA.sendChatMessage('Hello chers collègues ! Bienvenue au B1 comme si vous y étez (ou presque). ', 'Thomas');
+WA.sendChatMessage('Hello chers collègues ! Bienvenue au B1 comme si vous y étez (ou presque). ', 'Fabien');
 WA.sendChatMessage('Vous trouverez plusieurs petites surprises sur la carte, je vous laisse découvrir. Un salon est dispo juste à droite dans le grand bureau.' , 'Fabien');
-
+WA.sendChatMessage('Si vous voulez qu\'on garde contact twitter :  https://twitter.com/fabienrondeau', 'Fabien');
+WA.sendChatMessage('ou linkedin :  https://www.linkedin.com/in/fabienrondeau/', 'Fabien');
