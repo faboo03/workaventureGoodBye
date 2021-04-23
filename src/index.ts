f271d6ca235c408139a5d68c8f116c1409ad81eb
Fabien Rondeau <fabien.rondeau1@leroymerlin.fr>
2021-04-23 11:51:12 +0200

    correct script

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-let helloWorldPopup;
-
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
-
 // You can write your WorkAdventure script here, if any.
 // The "WA" global object is available from anywhere.
 
+let ok; 
 console.log('Script started successfully');
 //WA.openCoWebSite('https://workadventu.re');
 
